31bfd1921b69dcba95e39dc6fd1b2a68275bfa48
KateKmetko <kmetkoekaterina@gmail.com>
2018-02-28 01:53:06 +0300

    implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var length = str.length * bracketsConfig.length;
+  for(j = 0; j < length; j++){
+  for(i = 0; i < bracketsConfig.length; i++){
+  var str1 = bracketsConfig[i].join("");
+  str = str.replace(str1, "");
+  }
+  }
+
+  if(str.length > 0){
+  return false;
+  } else {
+  return true;
+  }
 }
